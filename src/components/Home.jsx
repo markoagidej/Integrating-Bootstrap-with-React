4bef90d859c4174e693fab66393586a07235c42d
Marko <markogidej@yahoo.com>
2024-07-15 22:42:45 -0400

    Q1T1 Done

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+    return (
+        <>
+            <Button variant="primary">Shop Now</Button>
+        </>
+    );
+};
+
+export default Home;
