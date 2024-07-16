b635afc0365bb36e6490bebedafdafaed55ff3c2
Marko <markogidej@yahoo.com>
2024-07-16 10:33:01 -0400

    Q2T1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+
+const NotFound = () => {
+    return (
+        <Container className="border border-danger">
+            404 Not Found
+        </Container>
+    );
+};
+
+export default NotFound;
