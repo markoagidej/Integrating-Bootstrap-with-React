d28937406f908165a4ced5da660edfc42f84bd35
Marko <markogidej@yahoo.com>
2024-07-16 11:40:04 -0400

    Q2T4 done

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import '../NotFound.css'
 
 const NotFound = () => {
     return (
-        <Container className="border border-danger">
-            404 Not Found
+        <Container>
+            <Col>
+                <Row>
+                    <Col>
+                        <Image className="border border-warning rounded" src='src/assets/404.jpg' fluid/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h1 className='fw-bold m-3'><strong>404 Not Found</strong></h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Use the link to the right to return home! -></p>
+                    </Col>
+                    <Col>
+                        <Link to='/' className='text-decoration-underline'>Return Home</Link>
+                    </Col>
+                </Row>
+            </Col>
         </Container>
     );
 };
