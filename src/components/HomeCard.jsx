2251a180fcdee07f44bba5f0de5b752f6baaf3b2
Marko <markogidej@yahoo.com>
2024-07-16 10:26:40 -0400

    Q1T4 done

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -4,10 +4,11 @@ import { Card } from 'react-bootstrap';
 const HomeCard = () => {
     return (
         <Card className="border border-dark">
+            <Card.Img variant="top" src="src/assets/welcome-image.jpg" fluid />
             <Card.Body>
                 <Card.Title className='bg-secondary text-white'>Thing Title!</Card.Title>
                 <Card.Text className='bg-dark text-white'>
-                    A whole bunch of text i na card about thing.
+                    A whole bunch of text in a card about thing.
                 </Card.Text>
             </Card.Body>
         </Card>
